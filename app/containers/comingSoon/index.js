67f0d658206a3a4cfb629465f7f34500a607a0de
Terry O'Toole <otooleterrence@gmail.com>
2018-03-10 15:50:45 -0500

    react-helmet for metadata and link previews

--- app/containers/comingSoon/index.js
+++ app/containers/comingSoon/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Lottie from 'react-lottie';
+import {Helmet} from 'react-helmet';
 import * as animationData from 'store/logo1.json';
 import Header from 'components/Header';
 
@@ -21,10 +22,30 @@ class comingSoonContainer extends Component {
 
     return (
       <main className={styles['near-black']}>
+        <Helmet
+          title="Coming Soon"
+          meta={[
+            { name: 'author', content: 'Terry' },
+
+            { name: 'twitter:card', content: 'summary' },
+            { name: 'twitter:site', content: 'otoole.media' },
+            { name: 'twitter:creator', content: '@pterryodactyl' },
+            { name: 'twitter:title', content: 'Coming Soon' },
+            { name: 'twitter:image', content: 'http://www.otoole.media/images/preview.png' },
+
+            { property: 'og:title', content: 'Coming Soon' },
+            { property: 'og:site_name', content: 'otoole.media' },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content: 'http://www.otoole.media/coming-soon/'},
+            { property: 'og:description', content: 'Coming Soon' },
+            { property: 'og:image', content: 'http://www.otoole.media/images/preview.png' },
+            { property: 'og:site_name', content: 'otoole media' },
+          ]}
+        />
         <Header />
-          <Lottie
-            options={defaultOptions}
-          />
+        <Lottie
+          options={defaultOptions}
+        />
       </main>
     );
   }
